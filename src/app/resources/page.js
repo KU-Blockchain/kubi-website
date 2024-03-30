a2405f6e0ed7f07f20f13ca753c08f97190170ad
Vegito2367 <tejaaditya.g@gmail.com>
2024-03-30 17:32:14 -0500

    Events Page aesthetic

--- src/app/resources/page.js
+++ src/app/resources/page.js
@@ -14,8 +14,15 @@ export default function ResourcesPage() {
       <Flex justifyContent="center" m={5}>
         <iframe width="50%" height="500px" src="https://e.notionhero.io/e1/p/5e2607b-d5e9d358c1eaca582376674d8498255"></iframe>
       </Flex>
+      <Center>
+        <Text color="blue.600" fontWeight={500} fontSize="3xl" align="center">
+            Click here to learn more!
+        </Text>
+      </Center>
+      <Center style={{marginBottom:"5px"}}>
+        <Button colorScheme="linkedin" onClick={() => window.open("https://www.notion.so/kublockchain/Foundations-of-Blockchain-Bootcamp-1e994c38ed8b44768504c2247ae770c2?pvs=4", "_blank")}>View Notion Page</Button>
+      </Center>
       
-      <Button onClick={() => window.open(link, "_blank")}>Where r u</Button>
       <Footer />
     </>
   );

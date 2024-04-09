e79fad95e4e5c4e00bbf3ccfe95ed606c1f8f0ab
Vegito2367 <tejaaditya.g@gmail.com>
2024-04-09 17:30:02 -0500

    Adding more components

--- src/app/membership/page.js
+++ src/app/membership/page.js
@@ -5,9 +5,26 @@ import Link from "next/link";
 import SolidColorButton from "../../components/SolidColorButton";
 import Footer from "@/components/footer";
 import {buttonStyle} from "@/components/globalStyles.js";
-import { Box, Center, Flex, Img, Text } from "@chakra-ui/react";
+import { Box, Center, Flex, Img, Text, Input, Textarea } from "@chakra-ui/react";
 import styles from "@/components/globalstyles.module.css";
+import { useState } from "react";
+
 export default function MembershipPage() {
+
+  const [valid, setValid] = useState(true);
+  function emailValidity(event){
+    let email = event.target.value;
+    if(email.includes('@') && email.includes('.')){
+      setValid(true);
+    }
+    else if(email === ""){
+      setValid(true);
+    }
+    else{
+      setValid(false);
+    }
+
+  }
   return (
     <>
       <Navbar />
@@ -21,9 +38,34 @@ export default function MembershipPage() {
         <div id="HorizontalLayout" className={styles.contactFormBase}>
           <Box id="FormHolder" style={{width:"50%"}}>
             <Text align="center" fontSize="3xl">Chatting with the Club!</Text>
-            <Text align="center" fontSize="2xl">Get in touch with us below!</Text>
-            <Box id="Form">
-              
+            <Text align="center" fontSize="2xl">Get in touch with us below!</Text><br></br>
+            <Box id="Form" style={{display:"flex", flexDirection:"column", rowGap:"2rem", paddingLeft:"3rem", paddingRight:"3rem"}}>
+              <Box name="InputDiv" style={{display:"flex", flexDirection:"row", justifyContent:"space-between"}}>
+
+                <Box style={{display:"flex", flexDirection:"column", rowGap:"10px"}}>
+                  First Name:
+                  <Input id="firstName" variant='filled' placeholder='First Name'/>
+                </Box>
+
+                <Box style={{display:"flex", flexDirection:"column", rowGap:"10px"}}>
+                  Last Name:
+                  <Input id="lastName" variant='filled' placeholder='Last Name'/>
+                </Box>
+              </Box>
+
+              <Box style={{display:"flex", flexDirection:"column", rowGap:"10px"}}>
+                  Email:
+                  <Input id="email" variant='filled' placeholder='yourname@example.com' onChange={emailValidity} isInvalid={!valid}/>
+              </Box>
+
+              <Box style={{display:"flex", flexDirection:"column", rowGap:"10px"}}>
+                  Message:
+                  <Textarea
+                    placeholder='Enter your message here...'
+                    size='md'
+                    variant='filled'
+                  />
+              </Box>
             </Box>
           </Box>
           <Box id="OptionalInfo">

d21d7c0950ca7f4b148af7475c2e1d43b6a9184e
Charlie Doherty <charlieedoherty@gmail.com>
2025-12-16 03:14:42 -0600

    Update Exec

--- src/components/about_components/CurrentExecTeam.jsx
+++ src/components/about_components/CurrentExecTeam.jsx
@@ -28,94 +28,104 @@ const CurrentExecTeam = () => {
 
   const currentExecs = [
     {
-      headshot: "/images/headshots/micah_borghese.JPG",
+      headshot: "/images/headshots/jahnvi_maddila.jpeg",
       title: "President",
-      name: "Micah Borghese",
-      shortBio: "Micah is a senior Honors student studying Computer Science and Mathematics. He was been involved with the KU Blockchain Institute since Fall 2023 but his interest in blockchain started long before then. In 8th grade, Micah gave a presentation on if Bitcoin would be the future of currency. Since then, he was explored developing on the blockchain, winning 3rd place at HackKU 2024 for his XRPL sidechain NFT supply chain solution.",
+      name: "Jahnvi Maddila",
+      shortBio: "Jahnvi is a Senior studying Computer Science and Business from Overland Park, Kansas. Jahnvi previously led outreach, marketing, and retention for KUBI.",
       longBio:
-        "Micah is a senior Honors student studying Computer Science and Mathematics. He was been involved with the KU Blockchain Institute since Fall 2023 but his interest in blockchain started long before then. In 8th grade, Micah gave a presentation on if Bitcoin would be the future of currency. Since then, he was explored developing on the blockchain, winning 3rd place at HackKU 2024 for his XRPL sidechain NFT supply chain solution.",
-      linkedin: "https://www.linkedin.com/in/micah-borghese/",
-      email: "mailto:micah@kublockchain.com",
+        "Jahnvi is a Senior studying Computer Science. She previously led outreach, marketing, and retention. Her research focuses on Spectrum Management Framework for Unmanned Aerial Systems. Jahnvi is especially excited about applications of blockchain in DeFi and social networks.",
+      linkedin: "https://www.linkedin.com/in/jahnvi-maddila-228359250/",
+      email: "mailto:jahnvi@kublockchain.com",
     },
     {
       headshot: "/images/headshots/alex_perez.jpeg",
       title: "VP of Business",
       name: "Alex Perez",
-      shortBio: "Alex is a sophmore majoring in business administration from Chino Hills, California. He is also the Director of Funding for the United Business Council (UBC) at KU.",
+      shortBio: "Alex is a Junior majoring in Business Administration from Chino Hills, California. Alex has been involved with KUBI since Fall 2024.",
       longBio:
-        "Alex is a sophmore majoring in business administration from Chino Hills, California. He is also the Director of Funding for the United Business Council (UBC) at KU.",
+        "Alex is a Junior majoring in Business Administration from Chino Hills, California. He is also the Director of Funding for the United Business Council (UBC) at KU and joined KUBI in Fall 2024.",
       linkedin: "",
       email: "mailto:alex@kublockchain.com",
     },
     {
-      headshot: "/images/headshots/jahnvi_maddila.jpeg",
+      headshot: "/images/headshots/nischay_rawal.png",
       title: "VP of Engineering",
-      name: "Jahnvi Maddila",
-      shortBio: "Jahnvi is the VP of Engineering at KU Blockchain Institute, previously leading outreach, marketing, and retention. Her research focuses on Spectrum Management Framework for Unmanned Aerial Systems. Jahnvi is especially excited about applications of blockchain in DeFi and social networks.",
+      name: "Nischay Rawal",
+      shortBio: "Nischay Rawal is an Honors Sophomore and SELF fellow dual majoring in Computer Science and Mathematics. He is from Overland Park, Kansas and has been involved with KUBI since Fall 2024.",
       longBio:
-        "Jahnvi is the VP of Engineering at KU Blockchain Institute, previously leading outreach, marketing, and retention. Her research focuses on Spectrum Management Framework for Unmanned Aerial Systems. Jahnvi is especially excited about applications of blockchain in DeFi and social networks.",
-      linkedin: "https://www.linkedin.com/in/jahnvi-maddila-228359250/",
-      email: "mailto:jahnvi@kublockchain.com",
-    },
-    {
-      headshot: "/images/headshots/kyle_spragg.jpg",
-      title: "Director of Development",
-      name: "Kyle Spragg",
-      shortBio: "Kyle Spragg will be a third year student at the University of Kansas this fall, studying Computer Science and Business. He is from Naperville, Illinois, and participates in athletics. He became an Academic All-American in his fourth year of high school sports, and pursued club lacrosse in college. Currently, Kyle is the Vice President of the Kansas Lacrosse Team and a Financial Analyst for the Rock Chalk Investment Club.",
-      longBio:
-        "Kyle Spragg will be a third year student at the University of Kansas this fall, studying Computer Science and Business. He is from Naperville, Illinois, and participates in athletics. He became an Academic All-American in his fourth year of high school sports, and pursued club lacrosse in college. Currently, Kyle is the Vice President of the Kansas Lacrosse Team and a Financial Analyst for the Rock Chalk Investment Club.",
-      linkedin: "",
-      email: "mailto:kyle@kublockchain.com",
-    },
-    {
-      headshot: "/images/headshots/emma_du.JPG",
-      title: "Director of Marketing and Communications",
-      name: "Emma Du",
-      shortBio: "Emma is a sophomore majoring in computer science and minoring in business. She has been involved in KU Blockchain Institute since Fall 2023. Emma is interested in blockchain's potential to create a more transparent and fair digital economy, as well as its applications in the healthcare industry. Outside of KUBI, she is a part of the KU SELF Program and plays the violin in the KU Symphony Orchestra. In her free time, she enjoys playing soccer and tennis.",
-      longBio:
-        "Emma is a sophomore majoring in computer science and minoring in business. She has been involved in KU Blockchain Institute since Fall 2023. Emma is interested in blockchain's potential to create a more transparent and fair digital economy, as well as its applications in the healthcare industry. Outside of KUBI, she is a part of the KU SELF Program and plays the violin in the KU Symphony Orchestra. In her free time, she enjoys playing soccer and tennis.",
-      linkedin: "https://www.linkedin.com/in/emma-du-/",
-      email: "mailto:emmadu@kublockchain.com",
+        "Nischay Rawal is an Honors Sophomore and SELF fellow dual majoring in Computer Science and Mathematics. He has been involved with the KU Blockchain Institute since Fall 2024 and is currently also involved with research on programming synthesis.",
+      linkedin: "https://www.linkedin.com/in/nischay-rawal-821902288/",
+      email: "mailto:nischay@kublockchain.com",
     },
     {
       headshot: "/images/headshots/caleb_hite.jpeg",
-      title: "Director of Outreach and Growth",
+      title: "Vice President of Education",
       name: "Caleb Hite",
-      shortBio: "Caleb is a Sophomore Computer Science student from Shawnee, Kansas. Caleb is a member of ACM and German Club. Caleb was also a part of the Kansas Blockchain Fellowship.",
+      shortBio: "Caleb is a Junior Computer Science student from Shawnee, Kansas. Caleb has been involved with KUBI since Summer 2024.",
       longBio:
-        "Caleb is a Sophomore Computer Science student from Shawnee, Kansas. Caleb is a member of ACM and German Club. Caleb was also a part of the Kansas Blockchain Fellowship.",
+        "Caleb is a Junior Computer Science student from Shawnee, Kansas. Caleb is a member of ACM and German Club. Caleb initially joined KUBI as a part of the Kansas Blockchain Fellowship in Summer 2024 and has been involved ever since.",
       linkedin: "",
       email: "mailto:caleb@kublockchain.com",
     },
     {
-      headshot: "/images/headshots/vatsa_pandey.jpg",
-      title: "Director of Research",
-      name: "Vatsa Pandey",
-      shortBio: "Vatsa is a freshman studying CS, Math, and Economics, Competitive programmer, Machine learning researcher and KU Blockchain's director of Research. His hobbies include robots and rockets.",
+      headshot: "/images/headshots/charlie_doherty.jpg",
+      title: "Director of Engineering",
+      name: "Charlie Doherty",
+      shortBio: "Charlie is a Junior in the Honors Program studying Computer Science with a Minor in Business. Charlie is from Overland Park, Kansas, and joined KUBI in Spring 2025.",
       longBio:
-        "Vatsa is a freshman studying CS, Math, and Economics, Competitive programmer, Machine learning researcher and KU Blockchain's director of Research. His hobbies include robots and rockets.",
-      linkedin: "https://www.linkedin.com/in/vatsadev/",
-      email: "mailto:vatsa@kublockchain.com",
+        "Charlie is a Junior in the Honors Program studying Computer Science with a Minor in Business. Charlie is from Overland Park, Kansas, and joined KUBI in Fall 2025. Outside of KUBI, he is a member of Alpha Tau Omega and is an Entreprenuer/Mentor in the Catalyst Accelerator.",
+      linkedin: "https://www.linkedin.com/in/charlie-doherty/",
+      email: "mailto:charlie@kublockchain.com",
     },
     {
-      headshot: "/images/headshots/nischay_rawal.png",
-      title: "Director of Education",
-      name: "Nischay Rawal",
-      shortBio: "Nischay Rawal is a current honors freshman and self fellow dual majoring in Computer Science and Mathematics. He has been involved with the KU Blockchain Institute since Fall 2024 and is now the Director of Education. Nischay is currently also involved with research on programming synthesis.",
+      headshot: "/images/headshots/audrey_karn.jpg",
+      title: "Director of Marketing",
+      name: "Audrey Karn",
+      shortBio: "Audrey is a Junior studying Business Administration from Overland Park, Kansas. Audrey joined KUBI in Fall 2025.",
       longBio:
-        "Nischay Rawal is a current honors freshman and self fellow dual majoring in Computer Science and Mathematics. He has been involved with the KU Blockchain Institute since Fall 2024 and is now the Director of Education. Nischay is currently also involved with research on programming synthesis.",
-      linkedin: "https://www.linkedin.com/in/nischay-rawal-821902288/",
-      email: "mailto:nischay@kublockchain.com",
-    },{
+        "Audrey is a Junior studying Business Administration from Overland Park, Kansas. Audrey joined KUBI in Fall 2025 and is an Entrepreneur/Mentor working with the Catalyst Accelerator to start a catering business.",
+      linkedin: "https://www.linkedin.com/in/audrey-karn-0ba1b2298/",
+      email: "mailto:audrey@kublockchain.com",
+    },
+    {
+      headshot: "/images/headshots/will_calhoun.jpg",
+      title: "Director of Research and Development",
+      name: "Will Calhoun",
+      shortBio: "Will Calhoun is a Junior studying Computer Science. He is from Overland Park, Kansas, and has been with KUBI since Spring 2025.",
+      longBio:
+        "Will Calhoun is a Junior studying Computer Science. He is from Overland Park, Kansas, and has been with KUBI since Spring 2025.",
+      linkedin: "",
+      email: "mailto:will@kublockchain.com",
+    },
+    {
+      headshot: "/images/headshots/emma_du.JPG",
+      title: "Director of Communications",
+      name: "Emma Du",
+      shortBio: "Emma is a Sophomore majoring in Computer Science with a minor in Business from Overland Park, Kansas. She is also a SELF Fellow and has been involved in KUBI since Fall 2023.",
+      longBio:
+        "Emma is a Sophomore majoring in Computer Science with a Minor in Business. She has been involved in KU Blockchain Institute since Fall 2023. Emma is interested in blockchain's potential to create a more transparent and fair digital economy, as well as its applications in the healthcare industry. Outside of KUBI, she is a SELF Fellow and plays the violin in the KU Symphony Orchestra. In her free time, she enjoys playing soccer and tennis.",
+      linkedin: "https://www.linkedin.com/in/emma-du-/",
+      email: "mailto:emmadu@kublockchain.com",
+    },
+    {
       headshot: "/images/headshots/wolfgang_sell.jpg",
       title: "Director of Education",
       name: "Wolfgang Sell",
-      shortBio: "Wolfgang is a freshman majoring in Cybersecurity Engineering from Shawnee, Kansas. His interest in decentralized technology began by participating in the Kansas Blockchain Fellowship, and has since been inspired to pass on that knowledge to others as Director of Education. His current research is in healthcare applications of LLMs, and is also an avid violist playing in the KU Symphony Orchestra and numerous other ensembles.",
+      shortBio: "Wolfgang is a SELF Fellow Sophomore Majoring in Cybersecurity Engineering from Shawnee, Kansas. Wolfgang joined KUBI in Summer 2024.",
       longBio:
         "Wolfgang is a freshman majoring in Cybersecurity Engineering from Shawnee, Kansas. His interest in decentralized technology began by participating in the Kansas Blockchain Fellowship, and has since been inspired to pass on that knowledge to others as Director of Education. His current research is in healthcare applications of LLMs, and is also an avid violist playing in the KU Symphony Orchestra and numerous other ensembles.",
       linkedin: "",
       email: "mailto:wolfgang@kublockchain.com",
-    },
+    },{
+      headshot: "/images/headshots/kyle_spragg.jpg",
+      title: "Director of Finance",
+      name: "Kyle Spragg",
+      shortBio: "Kyle Spragg is a Senior studying Computer Science and Business. He is from Naperville, Illinois, and has been with KUBI since Summer 2024.",
+      longBio:
+        "Kyle Spragg is a Senior studying Computer Science and Business. He is from Naperville, Illinois, and participates in athletics. He became an Academic All-American in his fourth year of high school sports, and pursued club lacrosse in college. Currently, Kyle is the President of the Kansas Lacrosse Team and a Financial Analyst for the Rock Chalk Investment Club.",
+      linkedin: "https://www.linkedin.com/in/kylespragg/",
+      email: "mailto:kyle@kublockchain.com",
+    }
   ];
 
   return (
